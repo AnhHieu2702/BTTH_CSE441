31d377dfbe40b28e73a89e5fa1603677d92f0fec
Anhhieu2702 <vungochieu099(@gmail.com>
2025-07-28 16:49:46 +0700

    process the minus button

--- Project_cal/app/src/main/java/com/example/project_cal/MainActivity.java
+++ Project_cal/app/src/main/java/com/example/project_cal/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         edt3 = findViewById(R.id.edtc);
 
         btncong = findViewById(R.id.btncong);
+        btntru = findViewById(R.id.btntru);
 
         btncong.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
                     int b = Integer.parseInt(edt2.getText().toString());
                     int tong = a + b;
                     edt3.setText(a + " + " + b + " = " + tong);
+                } catch (NumberFormatException e) {
+                    edt3.setText("Vui lòng nhập số hợp lệ");
+                }
+            }
+        });
+
+        btntru.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int a = Integer.parseInt(edt1.getText().toString());
+                    int b = Integer.parseInt(edt2.getText().toString());
+                    int hieu = a - b;
+                    edt3.setText(a + " - " + b + " = " + hieu);
                 } catch (NumberFormatException e) {
                     edt3.setText("Vui lòng nhập số hợp lệ");
                 }
