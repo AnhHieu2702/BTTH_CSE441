faa4e5a8684e0fddf0d2209984853b429cca4ef2
Anhhieu2702 <vungochieu099(@gmail.com>
2025-07-28 23:33:07 +0700

    add Fahrenheit -> Celsius Button

--- Temperature/app/src/main/java/com/example/temperature/MainActivity.java
+++ Temperature/app/src/main/java/com/example/temperature/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.temperature;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,40 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editTextCelsius, editTextFahrenheit;
+    Button btnToCelsius, btnToFahrenheit, btnClear;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main);  // gắn layout từ res/layout/activity_main.xml
+
+        // Gắn ID từ XML vào biến Java
+        editTextCelsius = findViewById(R.id.editTextCelsius);
+        editTextFahrenheit = findViewById(R.id.editTextFahrenheit);
+        btnToCelsius = findViewById(R.id.btnToCelsius);
+        btnToFahrenheit = findViewById(R.id.btnToFahrenheit);
+        btnClear = findViewById(R.id.btnClear);
+
+        // Nút chuyển Fahrenheit -> Celsius
+        btnToCelsius.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fStr = editTextFahrenheit.getText().toString();
+                if (!fStr.isEmpty()) {
+                    try {
+                        double f = Double.parseDouble(fStr);
+                        double c = (f - 32) * 5 / 9;
+                        editTextCelsius.setText(String.format("%.2f", c));
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(MainActivity.this, "Invalid Fahrenheit input", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter Fahrenheit value", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
+        
     }
 }
