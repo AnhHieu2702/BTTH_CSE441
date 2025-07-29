4dab7ba2a297ce8ec36c16a8168fbf636cbec1c3
Anhhieu2702 <vungochieu099(@gmail.com>
2025-07-30 00:35:47 +0700

    delare variables/catch events and algorithms for result

--- Intent2/app/src/main/java/com/example/intent2/ResultActivity.java
+++ Intent2/app/src/main/java/com/example/intent2/ResultActivity.java
@@ -1,6 +1,10 @@
 package com.example.intent2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
+    TextView txtKetQua;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        txtKetQua = findViewById(R.id.txtketqua);
+        btnBack = findViewById(R.id.btnBack);
+
+        Intent intent = getIntent();
+        double a = intent.getDoubleExtra("hsa", 0);
+        double b = intent.getDoubleExtra("hsb", 0);
+
+        String result;
+        if (a == 0) {
+            result = (b == 0) ? "Vô số nghiệm" : "Vô nghiệm";
+        } else {
+            double x = -b / a;
+            result = "x = " + x;
+        }
+        txtKetQua.setText(result);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
         });
     }
 }
