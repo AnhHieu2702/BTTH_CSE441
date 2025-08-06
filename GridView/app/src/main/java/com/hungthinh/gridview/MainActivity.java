21a09c9b5d8c64c1997279b0513011de9523bfb6
Anhhieu2702 <vungochieu099(@gmail.com>
2025-08-06 23:28:53 +0700

    cache events and convert layout/GridView

--- GridView/app/src/main/java/com/hungthinh/gridview/MainActivity.java
+++ GridView/app/src/main/java/com/hungthinh/gridview/MainActivity.java
@@ -1,24 +1,49 @@
 package com.hungthinh.gridview;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView selection;
+    GridView gridView;
+
+    String[] brands = {
+            "iPad", "iPhone", "New iPad",
+            "Samsung", "Nokia", "Sony Ericson",
+            "Q-Mobile", "HTC", "Blackberry",
+            "LG", "G Phone", "FPT - Phone",
+            "HK Phone"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        selection = findViewById(R.id.selection);
+        gridView = findViewById(R.id.gridView1);
+
+        // Gắn dữ liệu vào GridView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                brands
+        );
+        gridView.setAdapter(adapter);
+
+        // Thiết lập sự kiện click
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                selection.setText("You selected: " + brands[position]);
+            }
         });
     }
 }
