3217322d08d5d0fe01c62386b2e8e9fc7dc5bc8f
Anhhieu2702 <vungochieu099(@gmail.com>
2025-08-06 16:12:03 +0700

    process when click on url, will move to url

--- Intent11/app/src/main/java/com/hungthinh/intent11/MainActivity.java
+++ Intent11/app/src/main/java/com/hungthinh/intent11/MainActivity.java
@@ -1,24 +1,38 @@
 package com.hungthinh.intent11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnopen;
+    EditText edtlink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtlink = findViewById(R.id.edtlink);
+        btnopen = findViewById(R.id.btnopen);
+
+        btnopen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = edtlink.getText().toString().trim();
+                if (!url.startsWith("https://")) {
+                    url = "https://" + url;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
         });
     }
 }
--- Intent11/app/src/main/java/com/hungthinh/intent11/WebActivity.java
+++ Intent11/app/src/main/java/com/hungthinh/intent11/WebActivity.java
@@ -1,24 +1,25 @@
 package com.hungthinh.intent11;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class WebActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_web);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        WebView webView = findViewById(R.id.webview1);
+        if (uri != null) {
+            webView.loadUrl(uri.toString());
+        }
     }
 }
