3c8e82e00b5581abb075376a71f21c63ac14c482
Anhhieu2702 <vungochieu099(@gmail.com>
2025-08-06 16:46:03 +0700

    cache events for Add Work button

--- Note/app/src/main/java/com/hungthinh/note/MainActivity.java
+++ Note/app/src/main/java/com/hungthinh/note/MainActivity.java
@@ -38,6 +38,46 @@ public class MainActivity extends Activity {
         lv = findViewById(R.id.listView1);
         txtdate = findViewById(R.id.txtdate2);
 
-       
+        // Khởi tạo danh sách và adapter
+        arraywork = new ArrayList<>();
+        arrAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arraywork);
+        lv.setAdapter(arrAdapter);
+
+        // Hiển thị ngày hiện tại
+        Date currentDate = Calendar.getInstance().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        txtdate.setText("Hôm Nay: " + format.format(currentDate));
+
+        // Xử lý sự kiện khi bấm nút Add Work
+        btnwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String work = edtwork.getText().toString().trim();
+                String hour = edth.getText().toString().trim();
+                String minute = edtm.getText().toString().trim();
+
+                if (work.equals("") || hour.equals("") || minute.equals("")) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("Info missing");
+                    builder.setMessage("Please enter all information of the work");
+                    builder.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // nothing
+                        }
+                    });
+                    builder.show();
+                } else {
+                    String str = "+ " + work + " - " + hour + ":" + minute;
+                    arraywork.add(str);
+                    arrAdapter.notifyDataSetChanged();
+
+                    // Reset input
+                    edtwork.setText("");
+                    edth.setText("");
+                    edtm.setText("");
+                }
+            }
+        });
     }
 }
